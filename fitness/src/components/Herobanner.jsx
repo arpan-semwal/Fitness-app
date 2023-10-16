812de5680a66a2ac24affd9713ca877b47ec5df4
arpan-semwal <arpansemwal8@gmail.com>
2023-10-16 14:09:31 +0530

    image

--- fitness/src/components/Herobanner.jsx
+++ fitness/src/components/Herobanner.jsx
@@ -1,40 +1,53 @@
-import { Box, Typography, Button } from "@mui/material";
+import {  Typography,Button, Box } from "@mui/material";
 import HeroBannnerImage from "../assets/images/banner.png";
 import "../App.css"; // Import your CSS file
 
+
 const Herobanner = () => {
   return (
-    <Box
+
+    <div className="container">
+        <div className="text">
+       <Box
       sx={{
-        mt: { lg: "212px", xs: "70px" },
-        ml: { sm: "50px" },
-        display: "flex", // Add a display property to make the text and image side by side
-        alignItems: "center", // Center text vertically
-      }}
-      position="relative"
+        width: 300,
+        height: 300,
+        mt:{lg:'160px' , xs:'70px' , ml:{sm:'60px'}},
+       
+        
+      }} position="relative" 
       p="20px"
     >
-      <div>
-        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-          Fitness Club
+       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+         Fitness Club
         </Typography>
 
-        <Typography fontWeight={700} sx={{ fontSize: { lg: "44px", xs: "40px" } }}>
-          Sweat, Smile, <br /> and Repeat
-        </Typography>
+        <Typography  fontWeight="700" 
+      sx={{fontSize:{lg:'44px' , xs:'40px'}}}
+        >
+          Sweat , Smile <br/> and repeat
+       </Typography>
 
-        <Typography fontSize="22px" lineHeight="40px">
-          Check out the most effective websites
-        </Typography>
+        <Typography  fontSize="22px" lineHeight="35px" mb={3}>
+          Checkout the most effective excercise for a person
+       </Typography>
 
-        <Button variant="contained" href="#exercises" color="error">
-          Explore Exercise
-        </Button>
-      </div>
+        <Button variant="contained" color="success" href="#excercise" >Explore Success</Button>
+      
+       
+       </Box>
+    </div>
 
-      <img src={HeroBannnerImage} alt="banner" className="hero-banner-img" />
-    </Box>
+    <div className="image">
+    <img src={HeroBannnerImage} className="herobanner-img"/>
+    </div>
+    </div>
+
+
+  
+
+   
   );
-};
+}
 
 export default Herobanner;
